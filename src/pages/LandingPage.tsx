7968b26bde75c99de3d9b8915472107ab1b2acaa
Arvind Padyachi <152790749+arvindchief@users.noreply.github.com>
2025-05-11 02:26:11 +0530

    time

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -43,7 +43,7 @@ const LandingPage = () => {
 
   // Calculate target time (next midnight)
   const targetDate = new Date();
-  targetDate.setHours(02, 26, 0, 0);
+  targetDate.setHours(2, 27, 0, 0);
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center birthday-gradient relative overflow-hidden">
